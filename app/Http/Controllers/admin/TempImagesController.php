a6efa5595a5ab04dc7bacd0338d8cf6c5f70d74c
sikandarali2211 <122548079+sikandarali2211@users.noreply.github.com>
2023-12-29 01:50:09 +0500

    first commit

---
+++ app/Http/Controllers/admin/TempImagesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Models\TempImage;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TempImagesController extends Controller
+{
+    public function create(Request $request){
+        $image = $request->image;
+
+        if(!empty($image)){
+            $ext = $image->getClientOriginalExtension();
+            $newName = time().'.'.$ext;
+
+            $tempImage = new TempImage();
+
+            $tempImage->name = $newName;
+            $tempImage->save();
+
+            $image->move(public_path().'/temp',$newName);
+
+            return response()->json([
+                'status' =>true,
+                'image_id' =>$tempImage->id,
+                'message' => 'Image Upload Successfully'
+            ]);
+
+        }
+
+    }
+}
